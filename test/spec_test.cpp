86619215d23f3c937582c89b99675751d3a2013e
David Buzinski <davidbuzinski@gmail.com>
2025-03-23 12:29:21 -0400

    idk

--- test/spec_test.cpp
+++ test/spec_test.cpp
@@ -61,10 +61,12 @@ struct SpecTestParam {
 
   static std::vector<SpecTestParam> from_json_file(const std::string &file_name) {
     std::vector<SpecTestParam> params;
+    std::filesystem::path file_path(file_name);
+    file_path.make_preferred();
 
-    std::ifstream ifs(file_name, std::ios_base::binary);
+    std::ifstream ifs(file_path.string(), std::ios_base::binary);
     if (!ifs.is_open()) {
-      throw std::runtime_error("Could not open file: " + file_name);
+      throw std::runtime_error("Could not open file: " + file_path.string());
     }
     Json::Value root;
     ifs >> root;
