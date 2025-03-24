a4c49daca1ec77ee1b42730f13c40d24f36482ab
David Buzinski <davidbuzinski@gmail.com>
2025-03-23 23:52:36 -0400

    fix render_node

--- src/visitor/render_node.hpp
+++ src/visitor/render_node.hpp
@@ -9,51 +9,39 @@
 namespace mstch {
 
 class render_node {
- public:
+public:
   enum class flag { none, escape_html };
   render_node(render_context& ctx, flag p_flag = flag::none):
     m_ctx(ctx), m_flag(p_flag)
   {
   }
 
-  template<class T>
-  std::string operator()(const T&) const {
-    return "";
-  }
-
-  std::string operator()(const int& value) const {
-    return std::to_string(value);
-  }
-
-  std::string operator()(const double& value) const {
-    std::stringstream ss;
-    ss << value;
-    return ss.str();
-  }
-
-  std::string operator()(const bool& value) const {
-    return value ? "true" : "false";
-  }
-
-  std::string operator()(const lambda& value) const {
-    std::string lambda_result = value([this](const mstch::node& n) -> std::string {
-      render_node no_escape_visitor(m_ctx, flag::none);
-      return std::visit(no_escape_visitor, n);
-    });
-
-    if (lambda_result.find("{{") != std::string::npos) {
+  template<typename T>
+  std::string operator()(const T& value) const {
+    if constexpr(std::is_same_v<T, long long> || std::is_same_v<T, int>) {
+      return std::to_string(value);
+    } else if constexpr(std::is_same_v<T, double>) {
+      std::stringstream ss;
+      ss << value;
+      return ss.str();
+    } else if constexpr(std::is_same_v<T, bool>) {
+      return value ? "true" : "false";
+    } else if constexpr(std::is_same_v<T, lambda>) {
+      std::string lambda_result = value([this](const mstch::node& n) {
+        return mstch::visit(render_node(m_ctx), n);
+      });
+      
+      // Let template_type handle the parsing - it will tokenize if it contains mustache tags
       template_type interpreted{lambda_result};
-      return render_context::push(m_ctx).render(interpreted);
-    } else {
-      return (m_flag == flag::escape_html) ? html_escape(lambda_result) : lambda_result;
+      auto rendered = render_context::push(m_ctx).render(interpreted);
+      return (m_flag == flag::escape_html) ? html_escape(rendered) : rendered;
+    } else if constexpr(std::is_same_v<T, std::string>) {
+      return (m_flag == flag::escape_html) ? html_escape(value) : value;
     }
+    return {};
   }
 
-  std::string operator()(const std::string& value) const {
-    return (m_flag == flag::escape_html) ? html_escape(value) : value;
-  }
-
- private:
+private:
   render_context& m_ctx;
   flag m_flag;
 };
